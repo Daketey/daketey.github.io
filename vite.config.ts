2e4222968c23e92c8795fb95fa754373892a4f0a
daketey <ritul.mahapatra@gmail.com>
2025-03-10 11:31:49 +0530

    vite build final

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    emptyOutDir: true,
+    outDir: './docs',
+  },
   plugins: [react()]
 })
